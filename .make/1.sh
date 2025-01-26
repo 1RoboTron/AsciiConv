3a7b9d58214e164565226d8027a4afb413982943
Robo <robotron489@gmail.com>
2025-01-26 02:40:00 +0000

    tar.xz

--- .make/1.sh
+++ .make/1.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-tar -xf venv.tar.gz && ./venv/bin/python -m PyInstaller --onefile main.py -n asciiconv && mv ./dist/asciiconv ./
+tar -xf venv.tar.xz && ./venv/bin/python -m PyInstaller --onefile main.py -n asciiconv && mv ./dist/asciiconv ./
--- .make/2.sh
+++ .make/2.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-tar -xf venv_alt.tar.gz && ./venv/bin/python -m PyInstaller --onefile main.py -n asciiconv && mv ./dist/asciiconv ./
+tar -xf venv_alt.tar.xz && ./venv/bin/python -m PyInstaller --onefile main.py -n asciiconv && mv ./dist/asciiconv ./
